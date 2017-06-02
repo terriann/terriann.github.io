5cb40c8b50c380dcf4011ba82c6ba42e10f71862
Terri Ann <terri.ann.s@gmail.com>
2017-06-01 23:27:05 -0400

    Introduced my first vue component, importing stylesheets

---
+++ src/components/CoverPhotos.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="side-cover-wrapper full-screen visible-lg visible-md">
+    <div class="fslider" data-speed="3000" data-pause="5000" data-animation="fade" data-arrows="false" data-pagi="false" style="position: absolute; width: 100%; height: 100%; top: 0; left: 0; background-color: #333;">
+      <div class="flexslider" style="height: 100% !important;">
+        <div class="slider-wrap" style="height: inherit !important;">
+          <div class="slide full-screen force-full-screen" style="background: url('../static/images/1.jpg') center right; background-size: cover; height: 100% !important;"></div>
+          <div class="slide full-screen force-full-screen" style="background: url('../static/images/2.jpg') center right; background-size: cover; height: 100% !important;"></div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'coverphoto',
+  data () {
+    return {
+      msg: 'Welcome to Your *New* Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
--- src/main.js
+++ src/main.js
@@ -6,6 +6,8 @@ import router from './router'
 
 Vue.config.productionTip = false
 
+require('../node_modules/bootstrap/dist/css/bootstrap.css')
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
